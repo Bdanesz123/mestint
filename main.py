a838a80299a7b67f1ccb0951074a9699b7ae5757
Daniel <71514894+Bdanesz123@users.noreply.github.com>
2026-02-24 17:39:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,36 @@
-# Kancsók kapacitása
-kapacitasok = (3, 5, 8)
+class Hanoi:
+    def __init__(self, ke ,c):
+        self.kezdo = ke
+        self.cel = c
+        self.N = len(ke)
 
-# Kezdeti állapot (3L, 5L, 8L)
-kezdo_allapot = (0, 0, 8)
-
-# Cél liter
-cel = 4
-
-
-def kovetkezo_allapotok(allapot):
-    uj_allapotok = []
-    kancsok = list(allapot)
-
-    # Átöntés műveletek (i -> j)
-    for i in range(3):
-        for j in range(3):
-            if i == j:
-                continue
-
-            if kancsok[i] == 0:
-                continue  # nincs mit önteni
-
-            if kancsok[j] == kapacitasok[j]:
-                continue  # nincs hely a célkancsóban
-
-            uj = list(kancsok)
-            atontheto = min(kancsok[i], kapacitasok[j] - kancsok[j])
-            uj[i] -= atontheto
-            uj[j] += atontheto
-            uj_allapotok.append(tuple(uj))
-
-    return uj_allapotok
+    def celteszt(self,a):
+        return a == self.cel
 
 
-def rekurziv_kereses(aktualis_allapot, latogatott, ut):
-    # Cél ellenőrzése
-    if cel in aktualis_allapot:
-        return ut, aktualis_allapot
+    def rakovetkezo(self,a):
+        gyerekek = []
 
-    # Következő lépések bejárása
-    for uj_allapot in kovetkezo_allapotok(aktualis_allapot):
-        if uj_allapot in latogatott:
-            continue
+        for melyiket in range(0,self.N):
+            for hova in ['P','Q','R']:
+                tmp = True
+                if a[melyiket] != hova:
+                    for i in range(0,melyiket):
+                        if a[i] != a[melyiket] and a[i] != hova:
+                            pass
+                        else:
+                            tmp = False
+                            break
+                else:
+                    tmp = False
 
-        latogatott.add(uj_allapot)
-        eredmeny = rekurziv_kereses(uj_allapot, latogatott, ut + [uj_allapot])
-        if eredmeny is not None:
-            return eredmeny
+                if tmp:
+                    uj_allapot = list(a)
+                    uj_allapot[melyiket] = hova
+                    gyerekek.append(tuple(uj_allapot))
 
-    return None
-
-
-def megoldas():
-    latogatott = {kezdo_allapot}
-    eredmeny = rekurziv_kereses(kezdo_allapot, latogatott,[])
-
-    if eredmeny is None:
-        print("Nincs megoldás.")
-        return
-
-    ut, vegallapot = eredmeny
-    print("Megoldás lépései:")
-    for lepes in ut:
-        print(lepes)
-    print("Végállapot:", vegallapot)
+        return gyerekek
 
 
 if __name__ == '__main__':
-    megoldas()
+    feladat = Hanoi(('P','P','P','P','P'),('R','R','R','R','R'))
